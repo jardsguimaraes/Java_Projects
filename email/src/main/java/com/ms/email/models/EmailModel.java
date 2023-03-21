de542bc6e5fe04341e2dff68d789a0894e26dafe
Jards Guimarães <jardsguimaraes@hotmail.com>
2023-03-21 15:52:35 -0300

    Microservice para envio de e-mail

---
+++ email/src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,31 @@
+package com.ms.email.models;
+
+import java.time.LocalDateTime;
+
+import com.ms.email.enums.StatusEmail;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDataEmail;
+    private StatusEmail statusEmail;
+}
